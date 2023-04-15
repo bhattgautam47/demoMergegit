a620d28c19979e9d31ab79afe588489bbe55df51
Gautam Bhatt <bhatt.gautam47@gmail.com>
2023-04-15 10:26:21 +0530

    Added world1

--- src/gitdemoprojectPackage/HelloWorld.java
+++ src/gitdemoprojectPackage/HelloWorld.java
@@ -3,6 +3,6 @@ package gitdemoprojectPackage;
 public class HelloWorld {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World");
+		System.out.println("Hello World1");
 	}
 }
