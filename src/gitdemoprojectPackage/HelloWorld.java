cbd7b157a5973a67140c0abfc2ae087801e1d28f
Gautam Bhatt <bhatt.gautam47@gmail.com>
2023-04-15 10:32:35 +0530

    this is 1-2 issues

--- src/gitdemoprojectPackage/HelloWorld.java
+++ src/gitdemoprojectPackage/HelloWorld.java
@@ -3,6 +3,6 @@ package gitdemoprojectPackage;
 public class HelloWorld {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World issue1");
+		System.out.println("Hello World issue1-2");
 	}
 }
